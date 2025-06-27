f83b0f36898fd043d049700afd3be5ac283784c9
Josh Priddle <jpriddle@me.com>
2025-06-26 22:46:33 -0400

    Update lcars formula

--- Formula/lcars.rb
+++ Formula/lcars.rb
@@ -5,10 +5,12 @@ class Lcars < Formula
   url      "https://github.com/built-fast/lcars/archive/v#{version}.tar.gz"
   sha256   "fixme"
 
-  head "https://github.com/built-fast/lcars.git"
+  head "https://github.com/built-fast/lcars.git", branch: "main"
 
   def install
-    bin.install "bin/lcars"
-    # man1.install "share/man/man1/lcars.1"
+    zsh_completion.install "completions/_lcars" => "_lcars"
+    prefix.install ["bin", "completions", "libexec", "vendor"]
+    man1.install "share/man/man1/lcars.1"
+    share.install "share/lcars"
   end
 end
