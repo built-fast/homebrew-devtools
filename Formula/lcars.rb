9467cfda22d27dacfe1ba06ba646d6024895a3ef
Josh Priddle <jpriddle@me.com>
2025-07-05 03:25:47 +0000

    Updated lcars to v0.1.3

--- Formula/lcars.rb
+++ Formula/lcars.rb
@@ -1,9 +1,9 @@
 class Lcars < Formula
-  version "0.1.2"
+  version "0.1.3"
 
   homepage "https://github.com/built-fast/lcars"
   url      "https://github.com/built-fast/lcars/archive/v#{version}.tar.gz"
-  sha256   "3ee30e5b4f47656dbcdb2b7e70dd19141ce4ab5422e7f03f849ad970642ab780"
+  sha256   "b224262e37255f78da410f3b1f07d4ea76722c11193fc475c1c0abf4103be390"
 
   head "https://github.com/built-fast/lcars.git", branch: "main"
 
