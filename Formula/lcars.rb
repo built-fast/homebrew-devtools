0711f21009bc6f75e51f54d6205b38774083b8fc
Josh Priddle <jpriddle@me.com>
2025-09-15 11:33:09 -0400

    Add lcars formula description

--- Formula/lcars.rb
+++ Formula/lcars.rb
@@ -1,6 +1,7 @@
 class Lcars < Formula
   version "0.2.2"
 
+  desc     "Laravel CLI and Reusable Scripts"
   homepage "https://github.com/built-fast/lcars"
   url      "https://github.com/built-fast/lcars/archive/v#{version}.tar.gz"
   sha256   "d5d86c94e7e95728fd9a8e899d518d966e680a26cfae15c7b6e1ea78a25d0e98"
