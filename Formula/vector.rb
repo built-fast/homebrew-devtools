95bf94e0faa9d7fd1fb0d551e0324398ea13041e
Josh Priddle <jpriddle@me.com>
2026-02-01 11:47:14 -0500

    Prep for vector cli

---
+++ Formula/vector.rb
@@ -0,0 +1,33 @@
+class Vector < Formula
+  version "0.1.0"
+
+  desc "CLI for Vector Pro hosting platform"
+  homepage "https://github.com/built-fast/vector-cli"
+  license "MIT"
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "vector"
+  end
+
+  test do
+    assert_match "vector #{version}", shell_output("#{bin}/vector --version")
+  end
+end
