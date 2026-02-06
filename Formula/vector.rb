b69da40b13364f0e2a5c0574f6d3eacffb2c8581
Josh Priddle <jpriddle@me.com>
2026-02-06 04:11:49 +0000

    Updated vector to v0.1.0

--- Formula/vector.rb
+++ Formula/vector.rb
@@ -8,18 +8,18 @@ class Vector < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "b17f1a6e1f6b0d425006923e39e7132975cc6599b0dc6c3f9792759967dc9b56"
     else
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "8fa718c7aef68d4630dac8c2c2f3804d633aa37542da09a3972a967218755d95"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "461adc4247512b47c3e4ca0ae13d454128f600cca1bd188814be79ee395c3b1e"
     else
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "f00f60a71028d97f679ec90ab9b7c529eee5dc20cdd9726f0062fe7c086004a4"
     end
   end
 
