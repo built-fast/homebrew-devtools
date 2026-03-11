66f97e0d5e4bedd2a421a480c5446ac2d16a1621
Josh Priddle <jpriddle@me.com>
2026-03-11 17:09:18 +0000

    Updated vector to v0.5.0

--- Formula/vector.rb
+++ Formula/vector.rb
@@ -1,5 +1,5 @@
 class Vector < Formula
-  version "0.4.0"
+  version "0.5.0"
 
   desc "CLI for Vector Pro hosting platform"
   homepage "https://github.com/built-fast/vector-cli"
@@ -8,18 +8,18 @@ class Vector < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-apple-darwin.tar.gz"
-      sha256 "a286decf19803997abc8d6c841ba0c7858da698c3136de732ffcee51fb47a7fc"
+      sha256 "8823ddc0df855f33032f31f2c8d75006318783bd83f98626a1a6cd0142455712"
     else
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-apple-darwin.tar.gz"
-      sha256 "9a9a105c9787d618724a98a23795b45a2177887d404ea9738eedab3be0face07"
+      sha256 "c86f67f48d466d0725c993fbd140e7990cb49e54a74d3f3abcd584b35d9f0eb4"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "156c961c95bc49be9999b4e39bb2c7ba93fe05162a71821f90690bc59db3bd10"
+      sha256 "b318567feba06e909576f5c0c0ffb30329584b6bb8af339d2af6a24a01380016"
     else
       url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "90afa1c4800ed511c1cb1949d931b1bdadefb7664463ba2e0e151d73777368af"
+      sha256 "3fe9dc513042b02ca42605ffb68bb372b6f74c3d3d680f49f58cb86ebf89012e"
     end
   end
 
