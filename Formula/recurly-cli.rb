8854a14edad3cc10d152fe3259e14deca889984c
Josh Priddle <jpriddle@me.com>
2026-04-03 04:58:13 +0000

    Updated recurly-cli to v0.0.1

--- Formula/recurly-cli.rb
+++ Formula/recurly-cli.rb
@@ -8,18 +8,18 @@ class RecurlyCli < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/recurly-cli/releases/download/v#{version}/recurly-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "FIXME"
+      sha256 "a38e938e9d0f0f7afe60e31b5ad0b8c5d2c7fbf6a6a86652076d74fdef575da5"
     else
       url "https://github.com/built-fast/recurly-cli/releases/download/v#{version}/recurly-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "FIXME"
+      sha256 "38c6be78a9f4e11c08d06c7307c79d2ae1a102de256936317cd4672ee93aa756"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/recurly-cli/releases/download/v#{version}/recurly-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "FIXME"
+      sha256 "bc2eefa894fbf507c148a4316b5e43e5cd457d32a7b10eb8a47ba90d79d48903"
     else
       url "https://github.com/built-fast/recurly-cli/releases/download/v#{version}/recurly-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "FIXME"
+      sha256 "feed64aa65f71ae551cd99a9fd69267e71b8929f8e521e5be0c2589cbce1f6da"
     end
   end
 
