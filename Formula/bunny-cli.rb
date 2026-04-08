b038bffa009d56d2ceef26791885ff1c0f529823
Josh Priddle <jpriddle@me.com>
2026-04-08 19:29:12 +0000

    Updated bunny-cli to v0.0.1

--- Formula/bunny-cli.rb
+++ Formula/bunny-cli.rb
@@ -1,5 +1,5 @@
 class BunnyCli < Formula
-  version "0.0.0"
+  version "0.0.1"
 
   desc "Unofficial Bunny.net CLI"
   homepage "https://github.com/built-fast/bunny-cli"
@@ -8,18 +8,18 @@ class BunnyCli < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "FIXME"
+      sha256 "4cb39e551a990b698ba9afca55c28e2d6d3bc51c7950ba06bd337165335f3e1b"
     else
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "FIXME"
+      sha256 "d47ed76bb84d41034756c55e576b3bcf74382a8169d9626d23530d33a2634bbc"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "FIXME"
+      sha256 "2f3cac122e5f6a0f2e70d5a01d55e07f2ba9190bf37aa35b9e258496fa5c334b"
     else
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "FIXME"
+      sha256 "cf4ff2705c83d55cdc3068858aa1f48ae76f4b0e24f4b79239d0cd88297300c0"
     end
   end
 
