0dc623c178c7c2c199222e7c3fff87b7f7e592cb
Josh Priddle <jpriddle@me.com>
2026-05-21 23:36:19 -0400

    Update formula

--- Formula/vector.rb
+++ Formula/vector.rb
@@ -7,18 +7,18 @@ class Vector < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector_#{version}_darwin_arm64.tar.gz"
       sha256 "b923c91134cff9e5e5bcfa55b22ba4f98f6f27824826d246851ae4c3226e7548"
     else
-      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector_#{version}_darwin_amd64.tar.gz"
       sha256 "6cbbd0abe29f92ab2c081ff7c6ea3c0d98d0b1a8e96b5810cdbdeaa1e00a52d0"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector_#{version}_linux_arm64.tar.gz"
       sha256 "2853a67f13bbd4c88c10cdfc3d2476a485012dff9ce32da10de6d1df06fb02fb"
     else
-      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/built-fast/vector-cli/releases/download/v#{version}/vector_#{version}_linux_amd64.tar.gz"
       sha256 "fa12bbfa7e1fbf810be86cad5c5726bf9d6a25229ea05009961cc8343a330b18"
     end
   end
@@ -26,6 +26,7 @@ class Vector < Formula
   def install
     bin.install "vector"
     man1.install "man/man1/vector.1"
+    generate_completions_from_executable(bin/"vector", "completion")
   end
 
   test do
