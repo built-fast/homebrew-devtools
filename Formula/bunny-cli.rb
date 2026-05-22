9826c3b51c12ec1a2262598bf935e71f6172999c
Josh Priddle <jpriddle@me.com>
2026-05-22 13:17:51 +0000

    Updated bunny-cli to v0.0.2

--- Formula/bunny-cli.rb
+++ Formula/bunny-cli.rb
@@ -1,5 +1,5 @@
 class BunnyCli < Formula
-  version "0.0.1"
+  version "0.0.2"
 
   desc "Unofficial Bunny.net CLI"
   homepage "https://github.com/built-fast/bunny-cli"
@@ -8,18 +8,18 @@ class BunnyCli < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "4cb39e551a990b698ba9afca55c28e2d6d3bc51c7950ba06bd337165335f3e1b"
+      sha256 "da4abebc352b23d2db2e32ad919d32dcb7cf43aee9ecb9bb6277e84dcd30c879"
     else
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "d47ed76bb84d41034756c55e576b3bcf74382a8169d9626d23530d33a2634bbc"
+      sha256 "b2c4fdbc0f76b3676f9b0532a8f4b7780962cf20aa6ea288b875bb8d95a4485a"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "2f3cac122e5f6a0f2e70d5a01d55e07f2ba9190bf37aa35b9e258496fa5c334b"
+      sha256 "f8126dd256561b1930e3c68970aab77d9aa142779a9f22de7382c48389e154cb"
     else
       url "https://github.com/built-fast/bunny-cli/releases/download/v#{version}/bunny-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "cf4ff2705c83d55cdc3068858aa1f48ae76f4b0e24f4b79239d0cd88297300c0"
+      sha256 "374aa57491f514de3a94897a7604be33a639a507cd4bb86f0c110a8737a0c3a6"
     end
   end
 
